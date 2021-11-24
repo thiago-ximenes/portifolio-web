266914d30d62e0e05e7678edb31c8feed0a9db2f
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-24 16:46:00 -0300

    :rocket: lança trybetunes tentativa 1

---
+++ projetos/trybetunes/precache-manifest.30f0ee0174bd8458ac871b6bcece64f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f1cc4e3b27bc3d480c90bd616133e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "376ebcf3c5f21cdb6007",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "9adc1491d197db508c78",
+    "url": "/static/css/main.2a53aaf9.chunk.css"
+  },
+  {
+    "revision": "376ebcf3c5f21cdb6007",
+    "url": "/static/js/2.eb2ce92f.chunk.js"
+  },
+  {
+    "revision": "d27b898e17f4bfdf442ad5c828ce299e",
+    "url": "/static/js/2.eb2ce92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adc1491d197db508c78",
+    "url": "/static/js/main.b72d86e8.chunk.js"
+  },
+  {
+    "revision": "d79d35a7cea75eb66725",
+    "url": "/static/js/runtime-main.dc25a059.js"
+  }
+]);
