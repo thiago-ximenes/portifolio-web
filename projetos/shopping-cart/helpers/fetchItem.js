adbdd6238bb9845a5833697068812460820e64ee
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-03 10:24:03 -0300

    install react

--- projetos/shopping-cart/helpers/fetchItem.js
+++
@@ -1,9 +0,0 @@
-const fetchItem = (id) => fetch(`https://api.mercadolibre.com/items/${id}`)
-    .then((response) => response.json())
-    .catch((error) => error);
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    fetchItem,
-  };
-}
--- projetos/shopping-cart/helpers/saveCartItems.js
+++
@@ -1,6 +0,0 @@
-const saveCartItems = (productItem) => localStorage
-  .setItem('cartItems', productItem);
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
