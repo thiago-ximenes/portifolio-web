1bdb68c4938814ccccd45f9f3a99e90abbaf609b
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-13 18:00:09 -0300

    add opacity 0 effect when it pass on skills section on mobile setup

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
-import { IoIosArrowUp } from 'react-icons/io'
+import React, { useEffect, useState } from 'react';
+import { IoIosArrowUp } from 'react-icons/io';
 
 function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) {
+        setIsVisible(false)
+        window.removeEventListener('scroll', handleScroll)
+      } else {
+        setIsVisible(true)
+        window.addEventListener('scroll', handleScroll)
+      }
+    });
+
+    intersectionObserver.observe(document.querySelector('#about'));
+
+    return () => {
+      intersectionObserver.disconnect();
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(timeout);
+    }
+  }, [])
+
+  function scrollTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  function handleScroll() {
+    setIsVisible(true)
+    timeout();
+  }
+
+  function timeout() {
+    setTimeout(() => {
+      setIsVisible(false)
+    }, 1000)
+  }
+
   return (
-    <a
-      href='#header'
+    <button
+      type="button"
+      onClick={ scrollTop }
     >
       <IoIosArrowUp
-        className="cursor-pointer text-secondary
-        fixed bottom-2 right-2 z-10 w-10 h-10
-        bg-white rounded-full shadow-lg"
+        className={`cursor-pointer text-white
+        fixed bottom-2 right-2 z-12 w-12 h-12
+        bg-primary rounded-full shadow-lg
+        p-2 transition-opacity
+        ${isVisible ? 'opacity-100' : 'opacity-0'}`}
       />
-    </a>
+    </button>
   )
 }
 
